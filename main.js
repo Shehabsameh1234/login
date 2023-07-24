78e6b6b954ffa3a3094b26180c6349e5d0604d67
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-07-24 11:37:13 +0300

    update

--- main.js
+++ main.js
@@ -4,44 +4,48 @@ var passup = document.getElementById("password-signup")
 var btnup = document.getElementById("button-signup")
 
 
+
 var validname = /^[a-zA-Z ]{3,20}$/;
 var validemail = /^[\w-\.]+@([a-z]+\.)+[a-z]{2,4}$/;
 var validpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
 
 var signuparr = []
-if(localStorage.data!=null){
-signuparr=JSON.parse(localStorage.data)
+if (localStorage.data != null) {
+  signuparr = JSON.parse(localStorage.data)
 }
 
 function signupfun() {
 
-var validusername = validname.test(nameup.value)
-var validemailenter = validemail.test( emailup.value)
-var validpassword = validpass.test( passup.value)
+  var validusername = validname.test(nameup.value)
+  var validemailenter = validemail.test(emailup.value)
+  var validpassword = validpass.test(passup.value)
 
 
-    var objup = {
-        usernameup: nameup.value.trim().toLowerCase(),
-        emialsignup: emailup.value.trim().toLowerCase(),
-        passwordup: passup.value
-    }
-console.log(objup.usernameup)
+  var objup = {
+    usernameup: nameup.value.trim().toLowerCase(),
+    emialsignup: emailup.value.trim().toLowerCase(),
+    passwordup: passup.value
+  }
+  console.log(objup.usernameup)
 
-  if(validusername==true && validemailenter==true && validpassword==true){
+  if (validusername == true && validemailenter == true && validpassword == true) {
     signuparr.push(objup)
-    
+
   }
 
 
-//   var emailvalue=(signuparr[0])
- 
-// var values = Object.values(emailvalue);
-// console.log(values[0]);
+  //   var emailvalue=(signuparr[0])
+
+  // var values = Object.values(emailvalue);
+  // console.log(values[0]);
 
 
-    localStorage.setItem("data",JSON.stringify(signuparr))
+  localStorage.setItem("data", JSON.stringify(signuparr))
 }
+
+
+
 
 
 
