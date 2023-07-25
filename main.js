bcfe8034223aca22c9867923c373b8564d82154f
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-07-25 14:37:34 +0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+// get elemnts form html
 var nameup = document.getElementById("username-signup")
 var emailup = document.getElementById("email-signup")
 var passup = document.getElementById("password-signup")
@@ -11,14 +14,20 @@ var profile = document.getElementById("toprofile")
 var emailsign = document.getElementById("email-signin")
 var logreq = document.getElementById("requiredlog")
 var weluser = document.getElementById("welcomeuser")
+// get elemnts form html
 
+
+// regex code for validation
 var validname = /^[a-zA-Z ]{3,20}$/;
 var validemail = /^[\w-\.]+@([a-z]+\.)+[a-z]{2,4}$/;
 var validpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+// regex code for validation
 
 
+// create new acc and save it in local storage
 var signuparr = []
 if (localStorage.data != null) {
+  // get data from local storage
   signuparr = JSON.parse(localStorage.data)
 }
 
@@ -33,15 +42,16 @@ function signupfun() {
     emialsignup: emailup.value.trim().toLowerCase(),
     passwordup: passup.value
   }
-
   for (var i = 0; i < signuparr.length; i++) {
     var emailvalue = (signuparr[i])
     var values = Object.values(emailvalue)
     var newemail = values[1]
-
   }
 
+
   inputreq.style.display = "none"
+
+  // if data corret , make a new acc
   if (validusername == true && validemailenter == true && validpassword == true && newemail != emailup.value) {
     signuparr.push(objup)
     inputreq.style.display = "block"
@@ -52,30 +62,38 @@ function signupfun() {
     console.log(nameup.value)
     clearinputs()
   }
+
+  // if data  does nor corret , do not make a new acc
   else if (nameup.value == "" || emailup.value == "" || passup.value == "") {
     inputreq.style.display = "block"
     inputreq.innerHTML = "ALL INPUTS ARE REQUIRED"
     inputreq.style.color = "red"
 
   }
+  // if data  does nor corret , do not make a new acc
   else if (validusername == false || validemailenter == false || validpassword == false) {
     inputreq.style.color = "red"
     inputreq.style.display = "block"
     inputreq.innerHTML = "wrog data"
   }
+  // if data  does nor corret , do not make a new acc
   else if (newemail == emailup.value) {
     emailtaken.style.color = "red"
     emailtaken.style.display = "block"
     inputreq.style.display = "none"
   }
 
+  // send arr to lacal storage
   localStorage.setItem("data", JSON.stringify(signuparr))
 }
+// create new acc and save it in local storage
 
 
+// log in function and chek if data exist or not
 function loginfun() {
 
 
+  // chek if data exist or not 
   for (var i = 0; i < signuparr.length; i++) {
     var emailvalue = (signuparr[i])
     var values = Object.values(emailvalue)
@@ -89,7 +107,7 @@ function loginfun() {
   }
 
   if (emailsign.value == opj1.emailemail && passignin.value == opj1.namename) {
-    profile.setAttribute("href","home1.html")
+    profile.setAttribute("href", "home1.html")
   }
   else if
     (passignin.value == "" && emailsign.value == "") {
@@ -103,48 +121,35 @@ function loginfun() {
     logreq.style.display = "block"
     logreq.innerHTML = "wrong data"
   }
-
-
-   var emailsignvalue = emailsign.value
-  index = signuparr.findIndex(x => x.emialsignup == emailsignvalue);
- var indesofemailsignvalue = (signuparr[index])
- var valueobj = Object.values(indesofemailsignvalue)
- var nameof = valueobj[0]
-  
-console.log(nameof)
- 
-
 }
+// log in function and chek if data exist or not
 
 
-
-
-
-
-
-
-
+// clear inputs after signnup
 function clearinputs() {
   passup.value = ""
   emailup.value = ""
   nameup.value = ""
 }
+// clear inputs after signnup
 
 
-
-
+// icon in signin page to to convert password to text
 function iconsignin() {
   if (passignin.getAttribute("type") == "password") {
     passignin.setAttribute("type", "text")
   } else { passignin.setAttribute("type", "password") }
 }
+// icon in signin page to to convert password to text
 
 
+// icon in signup page to to convert password to text
 function iconsignup() {
   if (passup.getAttribute("type") == "password") {
     passup.setAttribute("type", "text")
   } else { passup.setAttribute("type", "password") }
 }
+// icon in signin page to to convert password to text
 
 
 
@@ -152,6 +157,17 @@ function iconsignup() {
 
 
 
+
+// function a7a() {
+
+//   var emailsignvalue = emailsign.value
+//   index = signuparr.findIndex(x => x.emialsignup == emailsignvalue);
+//   var indesofemailsignvalue = (signuparr[index])
+//   var valueobj = Object.values(indesofemailsignvalue)
+//   var nameof = valueobj[0]
+//   console.log(nameof)
+
+// }
 
 
 
